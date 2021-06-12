44ac19f6cf09a4d3a1e2a90d2569020d3a359efc
nicolimo86 <limonginicola@gmail.com>
2021-06-12 15:05:03 +0200

    install  virtualenvwrapper

---
+++ ansible/roles/ansible-infra-tools/files/.setenv.sh
@@ -0,0 +1,7 @@
+#!/bin/env bash
+
+# virtualenvwrapper
+export WORKON_HOME=$HOME/.virtualenvs
+export PROJECT_HOME=$HOME/Devel
+export VIRTUALENVWRAPPER_PYTHON=/usr/bin/python3.6
+source /usr/local/bin/virtualenvwrapper.sh
